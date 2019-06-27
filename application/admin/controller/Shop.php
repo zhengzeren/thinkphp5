d3ce0d4bd512c47250e5a8752805b6d378c6cdc3
zhengzeren <727937519@qq.com>
2019-06-27 18:05:28 +0800

    商品

---
+++ application/admin/controller/Shop.php
@@ -0,0 +1,15 @@
+<?php
+    namespace app\admin\controller;
+    use think\Controller;
+
+    class Shop extends Controller
+    {
+        public function index(){
+            return view();
+        }
+
+        public function shopadd(){
+            return view();
+        }
+    }
+ ?>
