e35f723ad2eaa425fae9d2aeeae404f785e4ea70
zhengzeren <727937519@qq.com>
2019-06-27 14:40:06 +0800

    INDEX css CONTROLLER ADMIN INDEX

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,11 @@
+<?php
+    namespace app\admin\controller;
+    use think\Cotroller;
+    use think\Db;
+    class Index extends Controller
+    {
+        public function index(){
+
+        }
+    }
+ ?>
