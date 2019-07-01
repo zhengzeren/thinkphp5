5f46c8a8313d264e5c3d1823cc1630fb6fdd37eb
zhengzeren <727937519@qq.com>
2019-07-02 03:11:51 +0800

    分类 分页没写好

--- application/admin/controller/Sort.php
+++ application/admin/controller/Sort.php
@@ -1,16 +1,22 @@
 <?php
     namespace app\admin\controller;
+    use app\admin\model\Type as TypeModel;
     use think\Controller;
-
-    class Sort extends Controller
+    class Sort extends Base
     {
         public function index(){
-            $arr=db('type')->field(['id','name','pid','path','state','concat(path,id)'=>'paixu'])->order('paixu')->select();
-            $arrpid=db('type')->field('pid')->select();
-            foreach ($arrpid as $v) {
+            //$arr=db('type')->field(['id','name','pid','path','state','concat(path,id)'=>'paixu'])->order('paixu')->select();
+            //$arrpid=db('type')->field('pid')->select();
+            //return view('',['arr'=>$arr,'newarr'=>$newarr]);
+            $list=TypeModel::field(['id','name','pid','path','state','concat(path,id)'=>'paixu'])->order('paixu')->select();
+            $list1=TypeModel::field('pid')->select();
+            $list2=TypeModel::where('pid','eq',0)->order('id desc')->paginate(4);
+            foreach ($list1 as $v) {
                 $newarr[]=$v['pid'];
             }
-            return view('',['arr'=>$arr,'newarr'=>$newarr]);
+            //dump($newarr);
+            $this->assign(['list'=>$list,'newarr'=>$newarr,'list2'=>$list2]);
+            return $this->fetch();
         }
         public function update(){
             $arr['id']=input('get.id');
