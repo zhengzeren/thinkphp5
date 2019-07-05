a1389c6605182b8d94aac03b496dc06bfd30b710
zhengzeren <727937519@qq.com>
2019-07-05 12:34:47 +0800

    商品遍历 删除 启用禁用

--- application/admin/controller/Shop.php
+++ application/admin/controller/Shop.php
@@ -1,11 +1,41 @@
 <?php
     namespace app\admin\controller;
+    use app\admin\model\Shop as ShopModel;
     use think\Controller;
 
     class Shop extends Base
     {
         public function index(){
-            return view();
+            $list=ShopModel::order('id desc')->paginate(5);
+            return view('',['list'=>$list]);
+        }
+
+        public function stateupdate(){
+            $result = new ShopModel;
+            $id = input('post.id');
+            $state = input('post.state');
+            if($state == 1){
+                 $state = 0;
+                 $res = ShopModel::where('id',$id)->update(['state'=>$state]);
+                 //dump($res);
+                 return "成功下架";
+             }else{
+                 $state = 1;
+                 $res = ShopModel::where('id',$id)->update(['state'=>$state]);
+                 //dump($res);
+                 return "上架成功";
+             }
+        }
+
+        public function del(){
+            $id = input('post.id');
+            $user = ShopModel::get($id);
+            if($user){
+                $user->delete();
+                return "删除成功";
+            }else{
+                return "删除的商品不存在";
+            }
         }
 
         public function shopadd(){
@@ -19,5 +49,6 @@
         public function pm(){
         	return view();
         }
+
     }
  ?>
